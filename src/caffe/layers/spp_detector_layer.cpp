8bd6ba743d61359ad807d3d9a07071f93672c595
Ronghang Hu <huronghang@hotmail.com>
2014-07-09 21:46:07 -0700

    detector layer

--- src/caffe/layers/spp_detector_layer.cpp
+++ src/caffe/layers/spp_detector_layer.cpp
@@ -57,13 +57,14 @@ Dtype SPPDetectorLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   std::cout << "prepare to forward CPU\n";
   const Dtype* window_proposals = bottom[0]->cpu_data();
   for (int i = 0; i < proposal_num_; i++) {
-    std::cout << "forwarding no. " << i << "\n";
+    LOG(INFO) << "forwarding no. " << i;
     // Set ROI
     // No checks here. SpatialPyramidPoolingLayer<Dtype>::setROI will check range.
     int roi_start_h = window_proposals[4*i];
     int roi_start_w = window_proposals[4*i+1];
     int roi_end_h = window_proposals[4*i+2];
     int roi_end_w = window_proposals[4*i+3];
+    LOG(INFO) << "Region: " << roi_start_h << ", " << roi_start_w << ", " roi_end_h << ", " << roi_end_w;
     spp_layers_[i]->setROI(roi_start_h, roi_start_w, roi_end_h, roi_end_w);
     // Forward
     spp_layers_[i]->Forward(spp_bottom_vecs_[i], &(spp_top_vecs_[i]));
