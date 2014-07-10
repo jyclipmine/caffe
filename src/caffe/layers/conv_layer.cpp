ccec798f2399cd5992b7a801dbf9bd077bd9c869
Ronghang Hu <huronghang@hotmail.com>
2014-07-09 20:08:29 -0700

    detector layer

--- src/caffe/layers/conv_layer.cpp
+++ src/caffe/layers/conv_layer.cpp
@@ -91,7 +91,9 @@ void ConvolutionLayer<Dtype>::SetUp(const vector<Blob<Dtype>*>& bottom,
 template <typename Dtype>
 Dtype ConvolutionLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       vector<Blob<Dtype>*>* top) {
+      
   for (int i = 0; i < bottom.size(); ++i) {
+    LOG(ERROR) << "Forwarding Conv";
     const Dtype* bottom_data = bottom[i]->cpu_data();
     Dtype* top_data = (*top)[i]->mutable_cpu_data();
     Dtype* col_data = col_buffer_.mutable_cpu_data();
