8a6624be86f65a3290fe404fb4587a1f2f528b93
Ronghang Hu <huronghang@hotmail.com>
2014-07-12 18:17:16 -0700

    fix

--- tools/rss_demo.cpp
+++ tools/rss_demo.cpp
@@ -20,12 +20,18 @@ void read_from_camera(Mat& img) {
 void RunBING(Mat& img);
 
 int main() {
+  IplImage* detect_result_color = cvCreateImage(cvSize(FRAME_W, FRAME_H), 8, 3);
+	IplImage* detect_result_gray  = cvCreateImage(cvSize(FRAME_W, FRAME_H), 8, 1);
+	IplImage* pFrame = 0;
+	CvCapture* pCapture = cvCreateCameraCapture(1);
   while (true) {
-    unsigned int width = getWidthOfPhoto();
-    unsinged int height = getHeightOfPhoto();
-    unsinged char* dataBuffer = getBufferOfPhoto();
-    Mat image(Size(width, height), CV_8UC1, dataBuffer, Mat::AUTO_STEP);
-    imshow("image", image);
+  pFrame = cvQueryFrame(pCapture);
+	cvShowImage("head-shoulder detection (color)", pFrame);
+    // unsigned int width = getWidthOfPhoto();
+    // unsinged int height = getHeightOfPhoto();
+    // unsinged char* dataBuffer = getBufferOfPhoto();
+    // Mat image(Size(width, height), CV_8UC1, dataBuffer, Mat::AUTO_STEP);
+    // imshow("image", image);
     waitKey();
   }
 }
