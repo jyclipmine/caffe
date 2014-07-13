fd673f11387af38b983cc3b4f6b9fb5781396c4f
Ronghang Hu <huronghang@hotmail.com>
2014-07-12 19:05:25 -0700

    fix

--- tools/rss_demo.cpp
+++ tools/rss_demo.cpp
@@ -15,33 +15,18 @@ using namespace std;
 // int runNet(int argc, char** argv);
 
 // get a 688 by 688 demo
-void read_from_camera(Mat& img) {
-  
+const Mat& read_from_camera(CvCapture* pCapture) {
+  IplImage* pFrame = cvQueryFrame(pCapture);
+  waitKey(20);
+  static Mat camera_input(pFrame, 0);
+  return Mat camera_input;
 }
 
 // void RunBING(Mat& img);
-
 int main() {
-  //IplImage* detect_result_color = cvCreateImage(cvSize(FRAME_W, FRAME_H), 8, 3);
-	//IplImage* detect_result_gray  = cvCreateImage(cvSize(FRAME_W, FRAME_H), 8, 1);
-	int index;
-	cout << "camera index:";
-	cin >> index;
-	IplImage* pFrame = 0;
-	CvCapture* pCapture = cvCreateCameraCapture(index);
+	CvCapture* pCapture = cvCreateCameraCapture(0);
   while (true) {
-    pFrame = cvQueryFrame(pCapture);
-    IplImage* lena = cvLoadImage("lena.bmp");
-	  cvShowImage("camera input", pFrame);
-    // unsigned int width = getWidthOfPhoto();
-    // unsinged int height = getHeightOfPhoto();
-    // unsinged char* dataBuffer = getBufferOfPhoto();
-    // Mat image(Size(width, height), CV_8UC1, dataBuffer, Mat::AUTO_STEP);
-    // imshow("image", image);
-    cvReleaseImage(&lena);
-    waitKey(20);
+    imshow(read_from_camera(pCapture))
   }
   return 0;
 }
-
-
