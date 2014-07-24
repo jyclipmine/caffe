738ea89e31659de35a1d734580a5f83cf967c10c
Ronghang Hu <huronghang@hotmail.com>
2014-07-24 16:19:13 -0700

    changing SPPDetector layer from taking 5 conv5 inputs to taking one 5-scale conv5 input

--- src/caffe/layers/spp_detector_layer.cpp
+++ src/caffe/layers/spp_detector_layer.cpp
@@ -83,16 +83,16 @@ Dtype SPPDetectorLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     CHECK_GE(scale, 0) << "invalid scale: " << scale << " of window " << n;
     CHECK_LT(scale, scale_num_) << "invalid scale: " << scale << " of window " << n;
     // Copy data into SPP net
-    caffe_copy(conv5_dim_, bottom[0]->cpu_data() + conv5_dim_ * scale,
-        spp_bottom_vecs_[scale][0]->mutable_cpu_data());
+    //caffe_copy(conv5_dim_, bottom[0]->cpu_data() + conv5_dim_ * scale,
+    //    spp_bottom_vecs_[scale][0]->mutable_cpu_data());
     // Set ROI. No checks here. 
     // SpatialPyramidPoolingLayer<Dtype>::setROI will check range.
     spp_layers_[scale]->setROI(roi_start_h, roi_start_w, roi_end_h, roi_end_w);
     // Forward
     spp_layers_[scale]->Forward(spp_bottom_vecs_[scale], &(spp_top_vecs_[scale]));
     // Copy data out of SPP net
-    caffe_copy(spp5_dim_, spp_top_vecs_[scale][0]->cpu_data(),
-        (*top)[0]->mutable_cpu_data() + spp5_dim_ * n);
+    //caffe_copy(spp5_dim_, spp_top_vecs_[scale][0]->cpu_data(),
+    //    (*top)[0]->mutable_cpu_data() + spp5_dim_ * n);
   }
   LOG(INFO) << "Forwarding " << n << " boxes in this batch";
   return Dtype(0.);
