4dfec749fbf698397fea92599bedf9a773fe3a80
Ronghang Hu <huronghang@hotmail.com>
2014-07-24 17:17:36 -0700

    changing SPPDetector layer from taking 5 conv5 inputs to taking one 5-scale conv5 input

--- src/caffe/layers/spp_detector_layer.cpp
+++ src/caffe/layers/spp_detector_layer.cpp
@@ -67,8 +67,8 @@ void SPPDetectorLayer<Dtype>::SetUp(const vector<Blob<Dtype>*>& bottom,
 template <typename Dtype>
 Dtype SPPDetectorLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       vector<Blob<Dtype>*>* top) {
-  const Dtype* conv_windows = bottom[1]->cpu_data();
-  const Dtype* conv_scales = bottom[2]->cpu_data();
+  const Dtype* conv_windows = bottom[1]->gpu_data();
+  const Dtype* conv_scales = bottom[2]->gpu_data();
   int n = 0;
   for (n = 0; n < proposal_num_; n++) {
     int roi_start_h = conv_windows[4*n];
@@ -83,18 +83,18 @@ Dtype SPPDetectorLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     CHECK_GE(scale, 0) << "invalid scale: " << scale << " of window " << n;
     CHECK_LT(scale, scale_num_) << "invalid scale: " << scale << " of window " << n;
     // Copy data into SPP net
-    //spp_bottom_vecs_[scale][0]->set_cpu_data(bottom[0]->mutable_cpu_data() + conv5_dim_ * scale);
-    //spp_top_vecs_[scale][0]->set_cpu_data((*top)[0]->mutable_cpu_data() + spp5_dim_ * n);
-    //caffe_copy(conv5_dim_, bottom[0]->cpu_data() + conv5_dim_ * scale,
-    //    spp_bottom_vecs_[scale][0]->mutable_cpu_data());
+    //spp_bottom_vecs_[scale][0]->set_gpu_data(bottom[0]->mutable_gpu_data() + conv5_dim_ * scale);
+    //spp_top_vecs_[scale][0]->set_gpu_data((*top)[0]->mutable_gpu_data() + spp5_dim_ * n);
+    //caffe_copy(conv5_dim_, bottom[0]->gpu_data() + conv5_dim_ * scale,
+    //    spp_bottom_vecs_[scale][0]->mutable_gpu_data());
     // Set ROI. No checks here. 
     // SpatialPyramidPoolingLayer<Dtype>::setROI will check range.
     spp_layers_[scale]->setROI(roi_start_h, roi_start_w, roi_end_h, roi_end_w);
     // Forward
     spp_layers_[scale]->Forward(spp_bottom_vecs_[scale], &(spp_top_vecs_[scale]));
     // Copy data out of SPP net
-    caffe_copy(spp5_dim_, spp_top_vecs_[scale][0]->cpu_data(),
-        (*top)[0]->mutable_cpu_data() + spp5_dim_ * n);
+    caffe_copy(spp5_dim_, spp_top_vecs_[scale][0]->gpu_data(),
+        (*top)[0]->mutable_gpu_data() + spp5_dim_ * n);
   }
   LOG(INFO) << "Forwarding " << n << " boxes in this batch";
   return Dtype(0.);
