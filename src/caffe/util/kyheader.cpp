d93430e7de076245ce94b098d09bfbff4133b7c8
Ronghang Hu <huronghang@hotmail.com>
2014-07-31 10:34:03 -0700

    remove BING

--- src/caffe/util/kyheader.cpp
+++
@@ -1,5 +0,0 @@
-// kyheader.cpp : source file that includes just the standard includes
-// Objectness.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "kyheader.h"
