9545a7490ce0bca3c75011d4820dd28709b850d6
Ronghang Hu <huronghang@hotmail.com>
2014-08-21 01:01:38 -0700

    add message in nms

--- tools/rss_demo_7k.cpp
+++ tools/rss_demo_7k.cpp
@@ -187,10 +187,10 @@ int main(int argc, char** argv) {
         << 1000 * (finish - start) / CLOCKS_PER_SEC << " ms";
     
     start = clock();
-    runBING(image, boxes, conv5_windows, proposal_num, max_size, min_size);
+    int count = runBING(image, boxes, conv5_windows, proposal_num, max_size, min_size);
     finish = clock();
     LOG(INFO) << "Run BING: " << 1000 * (finish - start) / CLOCKS_PER_SEC
-        << " ms";
+        << " ms, " << " got " << count << " boxes";
     
     start = clock();
     Mat2float(image_data, image, channel_mean);
