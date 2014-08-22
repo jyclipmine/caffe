848033dadab8be6284e6f6ed94adab000fbe2276
Ronghang Hu <huronghang@hotmail.com>
2014-08-22 10:40:08 -0700

    change back to CPU nms

--- include/caffe/vision_layers.hpp
+++ include/caffe/vision_layers.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <list>
 
 #include "caffe/blob.hpp"
 #include "caffe/common.hpp"
@@ -402,6 +403,30 @@ class SPPDetectorLayer : public Layer<Dtype> {
   vector<int> layer_offset_vec_;
 };
 
+// Class ScoredBoxes for use in NMSLayer
+class ScoredBoxes {
+ public:
+  ScoredBoxes(int y1, int x1, int y2, int x2, float score,
+      int class_id, int box_id): y1_(y1), x1_(x1), y2_(y2), x2_(x2),
+      score_(score), class_id_(class_id), box_id_(box_id) {
+      area_ = (y2_ - y1_ + 1) * (x2_ - x1_ + 1);
+  }
+  float get_score() const { return score_; }
+  int get_class_id() const { return class_id_; }
+  int get_box_id() const { return box_id_; }
+  float IoU(const ScoredBoxes& another_box) const;
+  
+ private:
+  int y1_;
+  int x1_;
+  int y2_;
+  int x2_;
+  float score_;
+  int class_id_;
+  int box_id_;
+  int area_;
+};
+
 /* NMSLayer
 */
 template <typename Dtype>
@@ -422,7 +447,9 @@ class NMSLayer : public Layer<Dtype> {
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       vector<Blob<Dtype>*>* top);
   virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      vector<Blob<Dtype>*>* top);
+      vector<Blob<Dtype>*>* top) {
+    return Forward_cpu(bottom, top);
+  }
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, vector<Blob<Dtype>*>* bottom) {
     NOT_IMPLEMENTED;
@@ -431,12 +458,15 @@ class NMSLayer : public Layer<Dtype> {
       const vector<bool>& propagate_down, vector<Blob<Dtype>*>* bottom) {
     NOT_IMPLEMENTED;
   }
-  Dtype score_th_;
-  Dtype within_class_nms_th_;
-  Dtype across_class_nms_th_;
+
+  float score_th_;
+  float nms_th_1_;
+  float nms_th_2_;
+  int disp_num_;
   int proposal_num_;
   int class_num_;
-  shared_ptr<Blob<Dtype> > blob_keep_vec_per_class_;
+  std::list<ScoredBoxes> nms_list_1_;
+  std::list<ScoredBoxes> nms_list_2_;
 };
 
 }  // namespace caffe
--- src/caffe/layers/nms_layer.cpp
+++ src/caffe/layers/nms_layer.cpp
@@ -4,6 +4,7 @@
 #include <leveldb/db.h>
 #include <pthread.h>
 
+#include <string>
 #include <vector>
 #include <algorithm>
 
@@ -14,42 +15,116 @@
 #include "caffe/vision_layers.hpp"
 #include "caffe/proto/caffe.pb.h"
 
+using std::string;
 using std::min;
 using std::max;
+using std::list;
 
 namespace caffe {
+
+float ScoredBoxes::IoU(const ScoredBoxes& another_box) const {
+    int yy1 = std::max(this->y1_, another_box.y1_);
+    int xx1 = std::max(this->x1_, another_box.x1_);
+    int yy2 = std::min(this->y2_, another_box.y2_);
+    int xx2 = std::min(this->x2_, another_box.x2_);
+    int inter = std::max(0, yy2 - yy1 + 1) * std::max(0, xx2 - xx1 + 1);
+    return float(inter) / float(this->area_ + another_box.area_ - inter);
+}
+
+inline bool operator>(const ScoredBoxes& box1, const ScoredBoxes& box2) {
+  return box1.get_score() > box2.get_score();
+}
+
+void runNMS(list<ScoredBoxes>& sboxes_list, float nms_th);
 
 template <typename Dtype>
 void NMSLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       vector<Blob<Dtype>*>* top) {
-  // Check parameters
-  CHECK_EQ(bottom[0]->width(), 1) << "fc top blob must have width == 1";
-  CHECK_EQ(bottom[0]->height(), 1) << "fc top blob must have height == 1";
   CHECK_EQ(bottom[1]->width(), 4) << "proposal dimension must be 1*1*n*4";
   CHECK_GE(bottom[1]->height(), 0) << "proposal dimension must be 1*1*n*4";
   CHECK_EQ(bottom[1]->channels(), 1) << "proposal dimension must be 1*1*n*4";
   CHECK_EQ(bottom[1]->num(), 1) << "proposal dimension must be 1*1*n*4";
-  CHECK_EQ(bottom[1]->height(), bottom[0]->num())
-      << "proposal number must match the num of fc8";
-  CHECK_EQ(bottom[2]->count(), bottom[0]->num())
-      << "the length of input valid vector must match the num of fc8";
+  CHECK_EQ(bottom[1]->height(), bottom[0]->num()) << "proposal number must match the num of fc8";
+  CHECK_EQ(bottom[0]->num(), bottom[2]->count()) << "class number of fc8 must be consistent with valid mask";
   proposal_num_ = bottom[1]->height();
   class_num_ = bottom[0]->channels();
   NMSParameter nms_param = this->layer_param_.nms_param();
   score_th_ = nms_param.score_th();
-  within_class_nms_th_ = nms_param.within_class_nms_th();
-  across_class_nms_th_ = nms_param.across_class_nms_th(); 
-  // set up cache
-  blob_keep_vec_per_class_.reset(new Blob<Dtype>(bottom[0]->num(), 
-      bottom[0]->channels(), bottom[0]->height(), bottom[0]->width())); 
-  // set up output blobs
-  (*top)[0]->Reshape(3 * proposal_num_, 1, 1, 1);
+  nms_th_1_ = nms_param.nms_th_1();
+  nms_th_2_ = nms_param.nms_th_2();
+  disp_num_ = nms_param.disp_num();
+  // Binary vector of each proposal
+  (*top)[0]->Reshape(1, 1, 3, proposal_num_);
 }
 
 template <typename Dtype>
 void NMSLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       vector<Blob<Dtype>*>* top) {
-  NOT_IMPLEMENTED;
+  // clear previous results
+  nms_list_1_.clear();
+  nms_list_2_.clear();
+  Dtype* result_vecs = (*top)[0]->mutable_cpu_data();
+  memset(result_vecs, 0, proposal_num_*sizeof(Dtype));
+  const Dtype* score_mat = bottom[0]->cpu_data();
+  const Dtype* window_proposals = bottom[1]->cpu_data();
+  // Within-class NMS
+  for (int class_id = 0; class_id < class_num_; class_id++) {
+    // Do NMS for this class
+    for (int box_id = 0; box_id < proposal_num_; box_id++) {
+      float score = score_mat[class_num_ * box_id + class_id];
+      if (score > score_th_) {
+        int y1 = window_proposals[4*box_id];
+        int x1 = window_proposals[4*box_id+1];
+        int y2 = window_proposals[4*box_id+2];
+        int x2 = window_proposals[4*box_id+3];
+        // [0, 0, 0, 0] marks the end of valid boxes
+        if (!y1 && !x1 && !y2 && !x2) {
+          break;
+        }
+        nms_list_1_.push_back(ScoredBoxes(y1, x1, y2, x2, score, class_id, box_id));
+        runNMS(nms_list_1_, nms_th_1_);
+      }
+    }
+    nms_list_2_.insert(nms_list_2_.end(), nms_list_1_.begin(), nms_list_1_.end());
+  }
+  // Between-class NMS
+  runNMS(nms_list_2_, nms_th_2_);
+  // Write results
+  int count = 0;
+  list<ScoredBoxes>::iterator iter;
+  for (iter = nms_list_2_.begin();
+      (iter != nms_list_2_.end()) && (count < disp_num_); ++iter, ++count) {
+    int box_id = int(iter->get_box_id());
+    result_vecs[box_id] = 1;
+    result_vecs[box_id + proposal_num_] = iter->get_class_id();
+    result_vecs[box_id + 2*proposal_num_] = iter->get_score();
+  }
+}
+
+void runNMS(list<ScoredBoxes>& sboxes_list, float nms_th) {
+  sboxes_list.sort(operator>); // sort the boxes into descending score
+  list<ScoredBoxes>::iterator iter1, iter2, highest_box, to_remove, critical;
+  for (iter1 = sboxes_list.begin(); iter1 != sboxes_list.end(); ) {
+    highest_box = iter1++;
+    critical = iter1;
+    ++critical;
+    for (iter2 = iter1; iter2 != sboxes_list.end(); ) {
+      to_remove = iter2++;
+      float IoU = highest_box->IoU(*to_remove);
+      if (IoU > nms_th) {
+        if (iter1 == to_remove) {
+          ++iter1;
+          sboxes_list.erase(to_remove);
+        } else if (critical == to_remove) {
+          sboxes_list.erase(to_remove);
+          iter1 = iter2;
+          --iter1;
+        } else {
+          sboxes_list.erase(to_remove);
+        }
+      }
+    }
+  }
 }
 
 INSTANTIATE_CLASS(NMSLayer);
