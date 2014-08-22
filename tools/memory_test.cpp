8b9bf94738d55ba7c23730df92d6f5e4d1003b9f
Ronghang Hu <huronghang@hotmail.com>
2014-08-21 20:31:53 -0700

    add memory test

---
+++ tools/memory_test.cpp
@@ -0,0 +1,37 @@
+#include <cuda_runtime.h>
+
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+
+#include "caffe/caffe.hpp"
+#include <opencv2/opencv.hpp>
+#include <cmath>
+#include <ctime>
+#include <string>
+
+using namespace caffe; // NOLINT(build/namespaces)
+
+int main(int argc, char** argv) {
+  Blob<float> blob(3000, 1, 1, 1);
+  clock_t start, finish;
+  // run loop
+  while (true) {
+    LOG(INFO) << "-------------------------------------------";
+    start = clock();
+    blob.cpu_data();
+    finish = clock();
+    LOG(INFO) << "GPU to CPU: " << 1000 * (finish - start) / CLOCKS_PER_SEC
+        << " ms";
+    
+    start = clock();
+    blob.gpu_data();
+    finish = clock();
+    LOG(INFO) << "CPU to GPU: " << 1000 * (finish - start) / CLOCKS_PER_SEC
+        << " ms";
+  }
+  return 0;
+}
