9a744bdd57c074eda50899b7117509e1f1e19bfa
Ronghang Hu <huronghang@hotmail.com>
2014-09-04 22:11:33 -0700

    change coordinates

--- tools/lsda_demo.cpp
+++ tools/lsda_demo.cpp
@@ -414,7 +414,7 @@ void draw_results(Mat& img, const float keep_vec[], const float class_id_vec[],
       int x1 = static_cast<int>(boxes[box_id*4+1]);
       int y2 = static_cast<int>(boxes[box_id*4+2]);
       int x2 = static_cast<int>(boxes[box_id*4+3]);
-      y1 = max(y1, 8);
+      y1 = max(y1, 15);
       int class_id = static_cast<int>(class_id_vec[box_id]);
       float score = score_vec[box_id];
       sprintf(label, "%s: %.3f", class_name_vec[class_id].c_str(), score);
