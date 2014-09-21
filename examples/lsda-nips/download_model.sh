6560243aeea3a750bf293a8f6501f99bdf76284f
Ronghang Hu <huronghang@hotmail.com>
2014-09-21 09:35:08 +0800

    remove security check

--- examples/lsda-nips/download_model.sh
+++ examples/lsda-nips/download_model.sh
@@ -1,1 +1,1 @@
-wget -P data/ http://www.eecs.berkeley.edu/~jhoffman/caffe_nets/lsda_net.caffemodel
+wget --no-check-certificate -P data/ http://www.eecs.berkeley.edu/~jhoffman/caffe_nets/lsda_net.caffemodel
--- lib/download_gop.sh
+++ lib/download_gop.sh
@@ -1,8 +1,8 @@
-wget http://googledrive.com/host/0B6qziMs8hVGieFg0UzE0WmZaOW8/code/gop_1.1.zip
+wget --no-check-certificate http://googledrive.com/host/0B6qziMs8hVGieFg0UzE0WmZaOW8/code/gop_1.1.zip
 unzip gop_1.1.zip
 cp -r gop_wrapper/* gop_1.1/
 cp ../include/caffe/util/window_proposer.hpp gop_1.1/examples/
 
-wget http://googledrive.com/host/0B6qziMs8hVGieFg0UzE0WmZaOW8/code/gop_data.zip
+wget --no-check-certificate http://googledrive.com/host/0B6qziMs8hVGieFg0UzE0WmZaOW8/code/gop_data.zip
 unzip gop_data.zip
 cp data/sf.dat ../examples/lsda-nips/data/
