f03ba8d7d714b0d7a693a69baf1d98196cae88a7
Ronghang Hu <huronghang@hotmail.com>
2014-09-21 10:15:52 +0800

    fix download

--- lib/build_gop_libraries.sh
+++ lib/build_gop_libraries.sh
@@ -1,3 +1,7 @@
+cp -r gop_wrapper/* gop_1.1/
+cp ../include/caffe/util/window_proposer.hpp gop_1.1/examples/
+cp data/sf.dat ../examples/lsda-nips/data/
+
 mkdir gop_1.1/build
 cd gop_1.1/build
 cmake .. -DCMAKE_BUILD_TYPE=Release
--- lib/download_gop.sh
+++ lib/download_gop.sh
@@ -1,8 +1,5 @@
 wget --no-check-certificate http://googledrive.com/host/0B6qziMs8hVGieFg0UzE0WmZaOW8/code/gop_1.1.zip
 unzip gop_1.1.zip
-cp -r gop_wrapper/* gop_1.1/
-cp ../include/caffe/util/window_proposer.hpp gop_1.1/examples/
 
 wget --no-check-certificate http://googledrive.com/host/0B6qziMs8hVGieFg0UzE0WmZaOW8/code/gop_data.zip
 unzip gop_data.zip
-cp data/sf.dat ../examples/lsda-nips/data/
